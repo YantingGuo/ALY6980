e659a2631b5fee4b3b12923ed94fad90811eba5a
yunpengxiao <yunpeng.shaw@gmail.com>
2020-03-13 18:05:52 -0700

    file upload support

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 from django.http import HttpResponse
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def index(request):
     return render(request, "index.html")
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage(os.path.join(BASE_DIR, "app/static/images/upload"))
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = '/static/images/upload/' + fs.url(filename)
+        return render(request, "file_upload.html", {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'file_upload.html')
